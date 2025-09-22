33b0af7aa5cb0ef31bc2709dc42817d3f862f91e
GamiDroid <kevin.d.damen@gmail.com>
2025-09-22 08:00:39 +0200

    Add workflow monitor pages

---
+++ WorkflowCore.Monitor/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using WorkflowCore.Monitor.Services;
+using WorkflowCore.Monitor.Workflows;
+
+namespace WorkflowCore.Monitor.Pages;
+
+public class IndexModel(
+    WorkflowMonitorService workflowMonitor,
+    IWorkflowHost host) : PageModel
+{
+    private readonly WorkflowMonitorService _workflowMonitor = workflowMonitor;
+    private readonly IWorkflowHost _host = host;
+
+    public IEnumerable<WorkflowInstance> WorkflowInstances { get; private set; } = [];
+    public IEnumerable<WorkflowDefinition> WorkflowDefinitions { get; private set; } = [];
+
+    public async Task OnGetAsync()
+    {
+        WorkflowInstances = await _workflowMonitor.GetWorkflowInstances();
+        WorkflowDefinitions = _workflowMonitor.GetRegisteredWorkflows();
+    }
+
+    public async Task<IActionResult> OnPostStartAsync(string id)
+    {
+        _ = await _host.StartWorkflow(id);
+
+        return RedirectToPage();
+    }
+}
---
+++ WorkflowCore.Monitor/Services/WorkflowMonitorService.cs
@@ -0,0 +1,27 @@
+﻿using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace WorkflowCore.Monitor.Services;
+
+public class WorkflowMonitorService(
+    IWorkflowRegistry registry,
+    IWorkflowRepository repository)
+{
+    private readonly IWorkflowRegistry _registry = registry;
+    private readonly IWorkflowRepository _repository = repository;
+
+    public Task<IEnumerable<WorkflowInstance>> GetWorkflowInstances()
+    {
+        return _repository.GetWorkflowInstances(null, null, null, null, 0, 100);
+    }
+
+    public Task<WorkflowInstance> GetWorkflowInstance(string id)
+    {
+        return _repository.GetWorkflowInstance(id);
+    }
+
+    public IEnumerable<WorkflowDefinition> GetRegisteredWorkflows()
+    {
+        return _registry.GetAllDefinitions();
+    }
+}
